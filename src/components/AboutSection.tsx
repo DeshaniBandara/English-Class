c4c300f4cfb6e9820b506003b724239b589a0cec
Deshani Bandara <piyumikabandara2001@gmail.com>
2026-08-31 10:58:00 +0530

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -4,7 +4,7 @@ import { Interactive3DCanvas } from './Interactive3DCanvas';
 import heroImg from '../assets/images/hero_kids_learning_1788130643393.jpg';
 import classroomImg from '../assets/images/modern_learning_space_1788130660467.jpg';
 import teacherPortraitImg from '../assets/images/teacher_ict_lab_session_1788134051659.jpg';
-import teacherLabImg from '../assets/images/teacher_ict_lab_session_1788134051659.jpg';
+import teacherLabImg from '../assets/images/IMG-20260820-WA0098.jpg';
 
 interface AboutSectionProps {
   onOpenJoinModal: () => void;
